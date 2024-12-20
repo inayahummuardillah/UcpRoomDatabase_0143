87a4b20d03a9c13d2ceb67a5b1788898c9edabb2
InayahUmmuArdillah <inayahummuardillah@gmail.com>
2024-12-21 00:25:54 +0630

    KrsApp berfungsi untuk menyimpan dan menginisialisasi instance ContainerApp yang bertanggung jawab untuk dependency injection.

---
+++ app/src/main/java/com/example/ucp2/KrsApp.kt
@@ -0,0 +1,16 @@
+package com.example.ucp2
+
+import android.app.Application
+import com.example.ucp2.dependenciesinjection.ContainerApp
+
+class KrsApp : Application() {
+    // Fungsinya untuk menyimpan instance ContainerApp
+    lateinit var containerApp: ContainerApp
+
+    override  fun onCreate() {
+        super.onCreate()
+        // Membuat instance ContainerApp
+        containerApp = ContainerApp(this)
+        // instance adalah object yang dibuat dari class
+    }
+}
